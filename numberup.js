63658ccd66a64c457feb5add760b3876a7db2c46
byoribyori <42292029+byoribyori@users.noreply.github.com>
2018-10-14 10:17:28 +0900

    Update numberup.js

--- numberup.js
+++ numberup.js
@@ -5,7 +5,33 @@
     var kaisuu = 0;
     var end = 0;
     var date = 0;
-
+    var o,v,oo,vv;
+    var haha = '';
+    var hyouji = true;
+    var ai = false;
+    var mm = null;
+    var ha = ['1234'];
+    for (var i = 1235; i < 9877; i++) {
+        var ii = i + '';
+        if (ii[0] === '0' || ii[1] === '0') {
+        } else {
+            if (ii[2] === '0' || ii[3] === '0') {
+            } else {
+                if (ii[0] === ii[1] || ii[0] === ii[2]) {
+                } else {
+                    if (ii[0] === ii[3] || ii[1] === ii[2]) {
+                    } else {
+                        if (ii[1] === ii[3] || ii[2] === ii[3]) {
+                        } else {
+                            ha.push(ii);;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    var h = [ha.length];
+    
     function ct() {
         var inputs = document.getElementsByTagName("input");
         for (var i = 0; i < inputs.length; i++) {
@@ -19,7 +45,7 @@
     while (ok === false) {
         var random = Math.floor(Math.random() * 10000 + 1);
         var mom = random + '';
-        if (random === 10000 || random < 1023) {
+        if (random === 10000 || random < 1234) {
         } else {
             if (mom[0] === '0' || mom[1] === '0') {
             } else {
@@ -41,9 +67,15 @@
     }
 
     button.onclick = () => {
+        if (yosou.value === '01' || ai === true) {
+            ai = true;
+            ct();
+            mm = ha[Math.floor(Math.random() * ha.length)]
+        }
         if (kaisuu === 0) {
             date = Date.now();
        }
+       if(ai === false){
         var mm = yosou.value + '';
         if (isNaN(yosou.value) === true || mm.length != 4) {
             alert('できません');
@@ -53,9 +85,6 @@
         if((mm[0] === '0' || mm[1] === '0') || (mm[2] === '0' || mm[3] === '0')){
             alert('0はありません');
             ct();
-            return;
-        }
-        if(end === 1){
             return;
         }
         var onaji = true;
@@ -73,33 +102,37 @@
             alert('同じ数は2回使えません');
             ct();
             return;
+        }}
+        if(end === 1){
+            return;
         }
-        var o = 0;
-        var v = 0;
+
+        o = 0;
+        v = 0;
 
         if (mom[0] === mm[0]) {
-            var o = o + 1;
+            o = o + 1;
         }
         if ((mom[0] === mm[1] || mom[0] === mm[2]) || mom[0] === mm[3]) {
-            var v = v + 1;
+            v = v + 1;
         }
         if (mom[1] === mm[1]) {
-            var o = o + 1;
+            o = o + 1;
         }
         if ((mom[1] === mm[0] || mom[1] === mm[2]) || mom[1] === mm[3]) {
-            var v = v + 1;
+            v = v + 1;
         }
         if (mom[2] === mm[2]) {
-            var o = o + 1;
+            o = o + 1;
         }
         if ((mom[2] === mm[0] || mom[2] === mm[1]) || mom[2] === mm[3]) {
-            var v = v + 1;
+            v = v + 1;
         }
         if (mom[3] === mm[3]) {
-            var o = o + 1;
+            o = o + 1;
         }
         if ((mom[3] === mm[0] || mom[3] === mm[1]) || mom[3] === mm[2]) {
-            var v = v + 1;
+            v = v + 1;
         }
 
         function result (){
@@ -121,9 +154,29 @@
             var paragragh = document.createElement('h4');
             paragragh.innerText += mm + ' 〇〇〇〇';
             resultDivided.appendChild(paragragh);
+            if (h[kaisuu] === 1) {
+                var paragragh = document.createElement('p');
+                paragragh.innerText += mm + 'しかありません';
+                resultDivided.appendChild(paragragh);
+            } else {
+                var paragragh = document.createElement('p');
+                paragragh.innerText += '全部で' + h[kaisuu] + '通りありました';
+                resultDivided.appendChild(paragragh);
+            }
             var paragragh = document.createElement('a');
             paragragh.href = 'https://byoribyori.github.io/number/numberup';
             paragragh.innerText += 'もう一度';
+            resultDivided.appendChild(paragragh);
+            var paragragh = document.createElement('br');
+            resultDivided.appendChild(paragragh);
+            var paragragh = document.createElement('button');
+            paragragh.id = 'miru';
+            paragragh.onclick = function () {
+                var paragragh = document.createElement('h5');
+                paragragh.innerText += haha;
+                resultDivided.appendChild(paragragh);
+            }
+            paragragh.innerText += '何通りか見る';
             resultDivided.appendChild(paragragh);
             end++
             setTimeout(result, 0);
@@ -154,12 +207,72 @@
                 paragragh.href = 'https://byoribyori.github.io/number/numberup';
                 paragragh.innerText += 'もう一度';
                 resultDivided.appendChild(paragragh);
+                var paragragh = document.createElement('br');
+                resultDivided.appendChild(paragragh);
+                var paragragh = document.createElement('button');
+                paragragh.id = 'miru';
+                paragragh.onclick = function () {
+                    var paragragh = document.createElement('h5');
+                    paragragh.innerText += haha;
+                    resultDivided.appendChild(paragragh);
+                }
+                paragragh.innerText += '何通りか見る';
+                resultDivided.appendChild(paragragh);
                 end++
                 setTimeout(xxx, 0);
             }
         }
         kaisuu++;
         ct();
+        oo = o;
+        vv = v;
+        var hale = ha.length;
+        for (e = 0; e < hale; e++) {
+            o = 0;
+            v = 0;
+            var hai = ha[0];
+            if (hai[0] === mm[0]) {
+                o = o + 1;
+            }
+            if ((hai[0] === mm[1] || hai[0] === mm[2]) || hai[0] === mm[3]) {
+                v = v + 1;
+            }
+            if (hai[1] === mm[1]) {
+                o = o + 1;
+            }
+            if ((hai[1] === mm[0] || hai[1] === mm[2]) || hai[1] === mm[3]) {
+                v = v + 1;
+            }
+            if (hai[2] === mm[2]) {
+                o = o + 1;
+            }
+            if ((hai[2] === mm[0] || hai[2] === mm[1]) || hai[2] === mm[3]) {
+                v = v + 1;
+            }
+            if (hai[3] === mm[3]) {
+                o = o + 1;
+            }
+            if ((hai[3] === mm[0] || hai[3] === mm[1]) || hai[3] === mm[2]) {
+                v = v + 1;
+            }
+
+            if (oo === o && vv === v) {
+                ha.push(hai);
+                ha.splice(0, 1);
+            } else {
+                ha.splice(0, 1);
+            }
+        }
+        h.push(ha.length);
+        if (hyouji != false && end === 0) {
+            haha += '\n' + kaisuu + '回目  ' + ha.length + '通り\n' + ha + '\n';
+        }
+        if (ha.length === 1) {
+            hyouji = false;
+        }
+        if(ai === true && end === 0){
+            button.onclick();
+        }
     }
 
     document.onkeydown = function (e) {
